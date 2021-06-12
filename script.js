b3bb1d90a0a7e51c491b8d1dacb00bd4fad5a472
Dorine van der Voorden <84196122+Dorinevandervoorden@users.noreply.github.com>
2021-06-12 08:14:26 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,80 @@
+console.log("Hallo Winc Academy Studenten");
+
+const age = 26;
+
+//Deze gaat over de leeftijd
+if(age >= 18 && age <= 25){
+
+    console.log("Je krijgt 50% korting");
+
+} else {
+
+    console.log("Sorry, maar je betaalt het volle pond. Graag of niet!")
+
+}
+
+//Deze gaat over de naam
+const name = 'Dorine';
+
+if(name == 'Bram' || name == 'Sarah'){
+
+    console.log("You get a beer. And you get a beer!!");
+
+} else {
+
+    console.log("Sorry, maar dat wordt betalen!");
+
+}
+
+//Deze gaat over de jubilieumkorting
+const totalAmount = 19;
+
+if (totalAmount >= 25 && totalAmount <50){
+
+    console.log("Jij krijgt bitterballetjesss");
+
+} else if (totalAmount >= 50 && totalAmount <100){
+
+    console.log("Jij krijgt gratis nachos!!");
+
+} else if (totalAmount >= 100){
+
+    console.log("Jij krijgt champagne. Topper!!");
+    
+} else {
+
+    console.log("Sorry, krent, maar je moet even iets meer betalen voordat je iets gratis krijgt....");
+
+}
+
+
+
+
+
+//Deze gaat over het geslacht
+const isFemale = true;
+
+if (isFemale) {
+
+    console.log("Hallo dames!!");
+
+}
+else {
+
+    console.log("Sorry heren, ladies only..!");
+
+}
+
+//Deze gaat over veilig thuiskomen
+const driverStatus = 'bob';
+
+if (driverStatus == 'bob') {
+
+    console.log("Jij mag rijden");
+}
+else {
+
+    console.log("Nee sorry, we regelen een taxi voor je");
+
+}
+
